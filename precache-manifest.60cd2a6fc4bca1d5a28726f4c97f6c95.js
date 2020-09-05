1fd8faa98d02eafb83d64bc785d93cc1d34277cb
Паша <holms703@gmail.com>
2020-09-06 01:43:09 +0300

    Updates

---
+++ precache-manifest.60cd2a6fc4bca1d5a28726f4c97f6c95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d23c6124585b22b07c3d7ca9c7232f2",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "1b1f2df1bf566fdb00df",
+    "url": "/goit-react-hw-02-feedback/static/js/2.45117538.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.45117538.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f37238ecb182c270a38",
+    "url": "/goit-react-hw-02-feedback/static/js/main.7fd345ba.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
